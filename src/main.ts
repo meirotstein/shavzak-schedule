959d976dcbf8b1aa87db20d6c9409c2caa641fc8
Meir Rotstein <mrotstein@fireblocks.com>
2024-09-12 00:17:53 +0300

    using Lara styled theme

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { createPinia } from "pinia";
-import PrimeVue from "primevue/config";
+import PrimeVue from 'primevue/config';
 import { createApp } from "vue";
 import App from "./App.vue";
-import Lara from "./presets/lara";
+import Lara from '@primevue/themes/lara';
 import "./style.css";
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
 app.use(PrimeVue, {
-  unstyled: true,
-  pt: Lara,
+  theme: {
+      preset: Lara,
+      options: {
+        darkModeSelector: '.fake-dark-selector', // force a non-usage of the dark mode
+    },
+  }
 });
 app.mount("#app");
