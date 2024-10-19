edec3e65b8e6797db079111dc2c723d627877512
Meir Rotstein <mrotstein@fireblocks.com>
2024-10-19 22:25:37 +0300

    start handling google data fetch

--- src/pages/SchedulePage.vue
+++ src/pages/SchedulePage.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { decodeCredential, GoogleLogin } from 'vue3-google-login';
 import PositionsTable from '../components/PositionsTable.vue';
 import SoldierList from '../components/SoldierList.vue';
+
+const route = useRoute();
+const spid = route.params.id;
+
+const callback = (response: any) => {
+  const userData = decodeCredential(response.credential);
+  console.log("Handle the userData", userData);
+}
+
 </script>
 
 <template>
   <div class="flex flex-col">
     <div class="flex flex-row">
-      <GoogleLogin :callback="(a: any) => console.log(a)" prompt auto-login />
+      <GoogleLogin :callback="callback" :error="callback" />
     </div>
     <div class="flex flex-row mt-6 space-x-4 h-full">
       <SoldierList />
