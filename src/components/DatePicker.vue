5fdba9eea82d4a80c07d018ecb9ffabb8c653133
Meir Rotstein <mrotstein@fireblocks.com>
2024-12-17 18:29:42 +0200

    datepicker - limit range and react to selections

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,22 +1,31 @@
 <script setup lang="ts">
 import DatePicker from "primevue/datepicker";
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useGAPIStore } from "../store/gapi";
+import { useScheduleStore } from "../store/schedule";
 
+const gapi = useGAPIStore();
+const scheduleStore = useScheduleStore();
 const selectedDate = ref();
+
+watch(selectedDate, (newValue) => {
+  scheduleStore.setScheduleDate(newValue);
+});
 
 </script>
 o
 <template>
   <div class="card flex flex-wrap gap-4" dir="ltr">
     <div class="flex-auto">
-      <DatePicker v-model="selectedDate" showIcon fluid :showOnFocus="false" inputId="selectedDate" :pt="{
-        pcPrevButton: {
-          root: 'datepicker-btn-rtl'
-        },
-        pcNextButton: {
-          root: 'datepicker-btn-rtl'
-        }
-      }" />
+      <DatePicker v-model="selectedDate" :minDate="gapi.presence?.start" :maxDate="gapi.presence?.end" showIcon fluid
+        :showOnFocus="false" inputId="selectedDate" :pt="{
+          pcPrevButton: {
+            root: 'datepicker-btn-rtl'
+          },
+          pcNextButton: {
+            root: 'datepicker-btn-rtl'
+          }
+        }" />
     </div>
   </div>
 </template>
