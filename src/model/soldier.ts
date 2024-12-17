88d83ad3b8f7013fd5c368ba496dbbd6a389dbf0
Meir Rotstein <mrotstein@fireblocks.com>
2024-12-17 23:30:55 +0200

    add presence array to solider model

--- src/model/soldier.ts
+++ src/model/soldier.ts
@@ -1,8 +1,11 @@
+import { IPresence } from "./presence";
+
 export interface ISoldier {
   id: string;
   name: string;
   role: string;
   platoon: string;
+  presence: Array<IPresence>;
 }
 
 export class SoldierModel implements ISoldier {
@@ -10,11 +13,17 @@ export class SoldierModel implements ISoldier {
   name: string;
   role: string;
   platoon: string;
+  presence: Array<IPresence> = [];
 
   constructor(id: string, name: string, role: string, platoon: string) {
     this.id = id;
     this.name = name;
     this.role = role;
     this.platoon = platoon;
+  }
+
+  addPresence(presence: IPresence): void {
+    this.presence.push(presence);
+    this.presence.sort((a, b) => a.date.getTime() - b.date.getTime());
   }
 }
--- test/model/soldier.test.ts
+++ test/model/soldier.test.ts
@@ -1,4 +1,6 @@
+import { parse } from "date-fns";
 import { describe, expect, test } from "vitest";
+import { PresenceModel, PresenceState } from "../../src/model/presence";
 import { SoldierModel } from "../../src/model/soldier";
 
 describe("Soldier model tests", () => {
@@ -9,5 +11,28 @@ describe("Soldier model tests", () => {
     expect(soldier.name).toBe("mose ufnik");
     expect(soldier.role).toBe("officer");
     expect(soldier.platoon).toBe("1");
+  });
+
+  test("Soldier model add presence - sort from the earliest to the latest", () => {
+    const soldier = new SoldierModel("123", "mose ufnik", "officer", "1");
+
+    const date1 = parse("2024-10-27", "yyyy-MM-dd", new Date());
+    const date2 = parse("2023-10-27", "yyyy-MM-dd", new Date());
+    const date3 = parse("2024-10-30", "yyyy-MM-dd", new Date());
+
+    soldier.addPresence(new PresenceModel(date1, "1"));
+    soldier.addPresence(new PresenceModel(date2, "0"));
+    soldier.addPresence(new PresenceModel(date3, "2"));
+
+    expect(soldier.presence.length).toBe(3);
+
+    expect(soldier.presence[0].presence).toBe(PresenceState.HOME);
+    expect(soldier.presence[0].date).toStrictEqual(date2);
+
+    expect(soldier.presence[1].presence).toBe(PresenceState.PRESENT);
+    expect(soldier.presence[1].date).toStrictEqual(date1);
+
+    expect(soldier.presence[2].presence).toBe(PresenceState.SICK);
+    expect(soldier.presence[2].date).toStrictEqual(date3);
   });
 });
