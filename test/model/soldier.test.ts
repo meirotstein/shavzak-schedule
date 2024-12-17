34155412d38f1c8e717fa2509c44a5e0ed9880cf
Meir Rotstein <mrotstein@fireblocks.com>
2024-12-17 14:27:42 +0200

    improve soldier test model

--- test/model/soldier.test.ts
+++ test/model/soldier.test.ts
@@ -3,10 +3,11 @@ import { SoldierModel } from "../../src/model/soldier";
 
 describe("Soldier model tests", () => {
   test("Soldier model initiation", () => {
-    const soldier = new SoldierModel("123", "mose ufnik", "officer");
+    const soldier = new SoldierModel("123", "mose ufnik", "officer", "1");
 
     expect(soldier.id).toBe("123");
     expect(soldier.name).toBe("mose ufnik");
     expect(soldier.role).toBe("officer");
+    expect(soldier.platoon).toBe("1");
   });
 });
