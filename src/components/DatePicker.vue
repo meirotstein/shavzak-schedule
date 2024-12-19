e43ce6df7b0e576a400dde9ac06dfe2d6950973f
Meir Rotstein <mrotstein@fireblocks.com>
2024-12-20 00:01:32 +0200

    sync date picker with 'date' query param

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { format, isValid, parse } from "date-fns";
 import DatePicker from "primevue/datepicker";
+import { computed, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import { useGAPIStore } from "../store/gapi";
 import { useScheduleStore } from "../store/schedule";
-import { computed } from "vue";
 
+const route = useRoute();
+const router = useRouter();
 const gapi = useGAPIStore();
 const scheduleStore = useScheduleStore();
 
@@ -11,6 +15,30 @@ const currentDate = computed({
   get: () => scheduleStore.scheduleDate,
   set: (value: Date) => scheduleStore.setScheduleDate(value),
 });
+
+if (route.query.date) {
+  handleDateInQuery(route.query.date as string);
+}
+
+// watch manual query changes
+watch(() => route.query, (newQuery) => {
+  if (!newQuery.date) return;
+  handleDateInQuery(newQuery.date as string);
+});
+
+// watch date picker changes
+watch(() => currentDate.value, (newDate) => {
+  if (!newDate) return;
+  router.replace({ query: { date: format(newDate, "yyyy-MM-dd") } });
+});
+
+function handleDateInQuery(dateStr: string) {
+  const date = parse(dateStr, "yyyy-MM-dd", new Date());
+  if (isValid(date)) {
+    currentDate.value = date;
+  }
+}
+
 
 </script>
 
